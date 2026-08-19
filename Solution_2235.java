f098b90e9c7a54a1481d59fcccd386d3a7dc121c
nihal pulikkal m5 air <nihalpulikkal82@gmail.com>
2026-08-19 23:05:48 +0530

    Solve LeetCode #2235 - Add Two Integers

---
+++ Solution_2235.java
@@ -0,0 +1,14 @@
+class Solution_2235 {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+        System.out.println(sol.sum(2, 3));
+
+    }
+}
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
